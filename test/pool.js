4329811d4f2c8e12bb07d3ffdb6263a3dbe9912e
decemberster <decemberster@bk.ru>
2018-04-04 22:26:06 +1000

    reogranize tests

--- test/pool.js
+++ test/pool.js
@@ -6,65 +6,40 @@ const path = require('path');
 const mdbPath = path.resolve(__dirname + '/media/Northwind2003.mdb');
 const connStr = 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' + mdbPath;
 
-describe('Connection', function() {
-    it('parseDateFn правильно парсит даты', function() {
-        const parseDateFn = require('../connection/speedup/parseDateFn');
-
-        let d1, d2;
-
-        d1 = parseDateFn('yyyy-MM-dd', 'H:m:s')('2018-01-05 12:34:56').toString();
-        d2 = new Date('2018-01-05 12:34:56').toString();
-        assert.equal(d1, d2);
-
-        d1 = parseDateFn('dd-MM-yyyy', 'H:m:s')('05-01-2018 12:34:56').toString();
-        d2 = new Date('2018-01-05 12:34:56').toString();
-        assert.equal(d1, d2);
-
-        d1 = parseDateFn('dd/MM/yyyy', 'H:m:s')('05/01/2018 12:34:56').toString();
-        d2 = new Date('2018-01-05 12:34:56').toString();
-        assert.equal(d1, d2);
-
-        d1 = parseDateFn('dd.MM.yyyy', 'H:m:s')('05.01.2018 12:34:56').toString();
-        d2 = new Date('2018-01-05 12:34:56').toString();
-        assert.equal(d1, d2);
-
-        d1 = parseDateFn('MM.dd.yyyy', 'H:m:s')('01.05.2018 12:34:56').toString();
-        d2 = new Date('2018-01-05 12:34:56').toString();
-        assert.equal(d1, d2);
-
-        d1 = parseDateFn('MM-dd-yyyy', 'H:m:s')('01-05-2018 12:34:56').toString();
-        d2 = new Date('2018-01-05 12:34:56').toString();
-        assert.equal(d1, d2);
-
-        d1 = parseDateFn('MM/dd/yyyy', 'H:m:s')('01/05/2018 12:34:56').toString();
-        d2 = new Date('2018-01-05 12:34:56').toString();
-        assert.equal(d1, d2);
-
-        d1 = parseDateFn('MM\\dd\\yyyy', 'H:m:s')('01\\05\\2018 12:34:56').toString();
-        d2 = '01\\05\\2018 12:34:56'; // can`t parse this format
-        assert.equal(d1, d2);
+describe('Pool', function() {
+    it('Pool создается и уничтожается', function() {
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
+        pool.end();
     });
 
-    it('Connection создается и уничтожается', function() {
-        const Connection = require('../connection/connection');
-
-        let connection = new Connection(connStr);
-
-        connection.connect((err, connection) => {
-            if (err) return console.error(err.message);
-
-            connection.end();
+    it('Pool выполняет sql-запрос', function(done) {
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
+        pool.query('SELECT 1+1 AS NUM;', (err, data) => {
+            if (err) {
+                done(err);
+            } else {
+                let fErr = false;
+                try {
+                    assert.deepEqual(data, [{NUM: 2}]);
+                } catch (err) {
+                    fErr = true;
+                    done(err);
+                }
+                if (!fErr) done(null);
+            }
+            pool.end();
         });
     });
 
     it('Правильно выполняется SQL-запрос с integer, string, float, boolean', function(done) {
-        const Connection = require('../connection/connection');
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
 
-        let connection = new Connection(connStr);
-
-        connection.query(
+        pool.query(
             'SELECT 2*2 AS intValue, "string" AS strValue, 3.14151926 AS floatValue, ' +
-                '1=1 AS trueValue, 1=0 AS falseValue;',
+                'cBool(1=1) AS trueValue, cBool(1=0) AS falseValue;',
             (err, data) => {
                 if (err) return done(err);
 
@@ -73,40 +48,38 @@ describe('Connection', function() {
                         {falseValue: 0, floatValue: 3.14151926, intValue: 4, strValue: 'string', trueValue: -1}
                     ]);
                 } catch (err) {
-                    connection.end();
+                    pool.end();
                     return done(err);
                 }
-                connection.end();
+                pool.end();
                 done(null);
             }
         );
     });
 
     it('Правильно выполняется SQL-запрос с datetime', function(done) {
-        const Connection = require('../connection/connection');
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
 
-        let connection = new Connection(connStr);
-
-        connection.query('SELECT #2018-01-01 00:00:00# AS dateValue;', (err, data) => {
+        pool.query('SELECT #2018-01-01 00:00:00# AS dateValue;', (err, data) => {
             if (err) return done(err);
 
             try {
                 assert.deepEqual(data[0]['dateValue'].getTime(), new Date('2018-01-01 00:00:00').getTime());
             } catch (err) {
-                connection.end();
+                pool.end();
                 return done(err);
             }
-            connection.end();
+            pool.end();
             done(null);
         });
     });
 
     it('Правильно обрабатываются синтаксические ошибки в SQL-запросе', function(done) {
-        const Connection = require('../connection/connection');
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
 
-        let connection = new Connection(connStr);
-
-        connection.query('syntax error', err => {
+        pool.query('syntax error', err => {
             if (err) {
                 //console.log(err.message);
                 if (err.message.indexOf('Microsoft JET Database Engine') >= 0) {
@@ -117,16 +90,16 @@ describe('Connection', function() {
             } else {
                 done(new Error());
             }
-            connection.end();
+            pool.end();
         });
     });
 
     it('Правильно выполняется SQL-запрос из файла', function(done) {
-        const Connection = require('../connection/connection');
-        let connection = new Connection(connStr);
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
 
         let filepath = path.join(__dirname, 'media/sql.sql');
-        connection.query(filepath, (err, data) => {
+        pool.query(filepath, (err, data) => {
             if (err) {
                 done(err);
             } else {
@@ -140,15 +113,15 @@ describe('Connection', function() {
 
                 if (!fErr) done(null);
             }
-            connection.end();
+            pool.end();
         });
     });
 
     it('Правильно выполняется подстановка именованных параметров', function(done) {
-        const Connection = require('../connection/connection');
-        let connection = new Connection(connStr);
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
 
-        connection.query(
+        pool.query(
             'SELECT :intValue AS intValue, :floatValue AS floatValue, :stringValue AS stringValue, :dateValue AS DateValue',
             {intValue: 42, floatValue: Math.PI, stringValue: 'arghhhhh', dateValue: new Date('2018-01-01 00:00:00')},
             (err, data) => {
@@ -171,8 +144,32 @@ describe('Connection', function() {
 
                 if (!fErr) done(null);
 
-                connection.end();
+                pool.end();
             }
         );
+    });
+
+    it('Правильно выполняется SQL-запрос с комментариями', function(done) {
+        const Pool = require('../pool/pool');
+        let pool = new Pool(connStr);
+
+        let sql = '/* check removing comments */SELECT 1 AS NUM1, -- number 1\n 2 AS NUM2 -- number 2';
+
+        pool.query(sql, (err, data) => {
+            if (err) {
+                done(err);
+            } else {
+                let fErr = false;
+                try {
+                    assert.deepEqual(data, [{NUM1: 1, NUM2: 2}]);
+                } catch (err) {
+                    fErr = true;
+                    done(err);
+                }
+
+                if (!fErr) done(null);
+            }
+            pool.end();
+        });
     });
 });
